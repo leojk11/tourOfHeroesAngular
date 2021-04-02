e677ffbeec1ffa1dc84969d567113c36ed5e2fa0
Leonid Jakimovski <ljakimovski211@yahoo.com>
2021-04-02 14:56:24 +0200

    added message service into hero service and sending some messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,16 +9,23 @@ import { Hero } from './hero';
 // mock heroes list
 import { HEROES } from './mock-heroes';
 
+// messages service
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messagesService: MessageService) { }
 
   // get heroes function, that returns mock heroes for now
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+
+    // sending message to the message service when the heroes get fetched
+    this.messagesService.add('Hero service: fetched heroes');
+
     return heroes;
   }
 }
