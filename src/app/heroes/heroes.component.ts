b8f53469aac676b91ccce4acb227a23b46d8f37a
Leonid Jakimovski <ljakimovski211@yahoo.com>
2021-04-02 15:52:18 +0200

    updated heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,9 +6,6 @@ import { Hero } from '../hero';
 // hero service
 import { HeroService } from '../hero.service';
 
-// messages service
-import { MessageService } from '../message.service';
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -18,26 +15,13 @@ export class HeroesComponent implements OnInit {
   // list of heroes
   heroes: Hero[] = [];
 
-  // selected hero for showing hero detail
-  selectedHero?: Hero; // on the start of the application there is no hero selected
-
   constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
+    private heroService: HeroService
   ) { }
 
   ngOnInit(): void {
     // call the getHeroes function 
     this.getHeroes();
-  }
-
-  // setting the selected hero
-  onSelect(hero: Hero) {
-    // setting the selected hero
-    this.selectedHero = hero;
-
-    // adding message when hero is clicked
-    this.messageService.add(`Heroes component: Selected hero id=${hero.id}`);
   }
 
   // get heroes from hero service
